a7492f7fd18d8c4efef5111b5711d7f07d3a8308
Gustavo Souza <gustavosouzacco@gmail.com>
2021-01-26 17:59:14 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,34 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // delete
 // ======================================
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(
-        req.body
-    );
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-
-    res.status(201).send(
-        {
-            id: id,
-            item: req.body
-        }
-    );
-});
-
-const del = router.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
-
-    res.status(200).send(
-        {
-            id: id,
-            item: req.body
-        }
-    );
-});
-
 app.use('/', index);
 app.use('/products', create);
 app.use('/products', put);
