b32f9287ac65a0f51ca3c3d2e2708b1273326971
Gustavo Souza <gustavosouzacco@gmail.com>
2021-01-26 13:26:51 -0300

    Criando APIs com NodeJs - Aula 7

--- bin/server.js
+++ bin/server.js
@@ -1,28 +1,19 @@
 'use strict'
 
+const app = require('../src/app');
 const http = require('http');
 const debug = require('debug')('nodestr:server');
-const express = require('express');
 
-const app = express();
 const port = normilizePort(process.env.PORT || '3000');
+
+const server = http.createServer(app);
 
 app.set('port', port);
 
-const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-app.use('/', route);
-
 server.listen(port);
+
 server.on('error', onError);
+server.on('listening', onListening);
 
 console.log("Pai ta on! Na porta: " + port);
 
@@ -61,4 +52,11 @@ function onError(error){
         default:
             throw error;
     }
+}
+
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string'? 'pipe ' + addr: 'port ' + addr.port;
+
+    debug('Listening on ' + bind);
 }
